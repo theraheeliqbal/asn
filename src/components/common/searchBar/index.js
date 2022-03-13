fd12031d30096a8202c4b4e07fe02709ad0643ba
theraheeliqbal <theraheeliqbal@gmail.com>
2022-03-13 18:42:14 +0500

    removed extra files and added wia-aria features

--- src/components/common/searchBar/index.js
+++ src/components/common/searchBar/index.js
@@ -4,16 +4,18 @@ import "./styles.css";
 function SearchBar({ onSearch }) {
   return (
     <div className="search-field">
-      <SearchIcon cn="search-icon" />
-      <label for="search-input-field" class="dn">
-        Search:{" "}
-      </label>
-      <input
-        type="text"
-        onChange={(e) => onSearch(e.target.value)}
-        id="search-input-field"
-        name="lname"
-      />
+      <form role="search">
+        <SearchIcon cn="search-icon" />
+        <label for="search-input-field" class="dn">
+          Search
+        </label>
+        <input
+          type="text"
+          onChange={(e) => onSearch(e.target.value)}
+          id="search-input-field"
+          name="search input"
+        />
+      </form>
     </div>
   );
 }
